d22c560e776d9a5234b88e777e57ef85388ed4d5
Mahek Desai <mahekdesai2001@gmail.com>
2024-04-29 17:51:14 -0700

    caught up till 04/26/2024

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from './auth.service';
+import { LoginRequest } from './login-request';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +14,30 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit{
+  constructor(private authService : AuthService, private router : Router){}
+
   ngOnInit(): void {
     this.form = new FormGroup({
       userName : new FormControl("", Validators.required),
-      password : new FormControl("", Validators.required),
+      password : new FormControl("", [Validators.required, Validators.pattern(this.passwordPattern)]),
     });
   }
+
+  private passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\\d\\X])(?=.*[!@#$%^&*]).{8,}$";
+
   form!: UntypedFormGroup;
 
   onSubmit(){
-    
+    let loginRequest : LoginRequest = <LoginRequest>{
+      userName : this.form.controls["userName"].value,
+      password : this.form.controls["password"].value,
+    };
+    this.authService.Login(loginRequest).subscribe({
+      next: result => {
+        console.log(result.message);
+        this.router.navigate([])
+      },
+      error: error => console.error(error)
+    });
   }
 }

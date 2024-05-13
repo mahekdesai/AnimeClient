8a2d3c9a303f0030c9f83b7c5cc9a160df107fc9
Mahek Desai <mahekdesai2001@gmail.com>
2024-05-13 13:43:40 -0700

    new anime data entry done

--- src/app/animes/animes.component.ts
+++ src/app/animes/animes.component.ts
@@ -4,17 +4,28 @@ import { Router, RouterLink } from '@angular/router';
 import { environment } from '../../environments/environment.development';
 import { Anime } from './anime';
 import { AuthenticationService } from '../auth/oktaauth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+interface NewAnime {
+  animeName: string;
+  animeImage: File | null;
+}
 
 @Component({
   selector: 'app-animes',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './animes.component.html',
   styleUrl: './animes.component.scss',
 })
 export class AnimesComponent implements OnInit {
   public animes: Anime[] = [];
   public isAuthorized : boolean = false;
+  public showForm: boolean = false;
+  public newAnime: NewAnime = {
+    animeName: '',
+    animeImage: null,
+  }
 
   constructor(private http: HttpClient, private authService : AuthenticationService, private router : Router) {}
 
@@ -39,5 +50,30 @@ export class AnimesComponent implements OnInit {
 
   checkAuthorization(): void {
     this.isAuthorized = this.authService.isAuthorized();
+  }
+
+  showAddAnimeForm(): void {
+    this.showForm = true;
+  }
+
+  handleFileInput(event: any): void {
+    const file = event.target.files[0];
+    this.newAnime.animeImage = file;
+  }
+
+  addNewAnime(): void {
+    const formData = new FormData();
+    formData.append('animeName', this.newAnime.animeName);
+    if (this.newAnime.animeImage) {
+      formData.append('animeImage', this.newAnime.animeImage);
+    }
+
+    this.http.post(environment.baseUrl + 'api/Animes', formData).subscribe({
+      next: () => {
+        this.getAnimes();
+        this.showForm = false;
+      },
+      error: (error) => console.error(error),
+    });
   }
 }
